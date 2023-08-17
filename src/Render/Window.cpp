3bccdc088aa40eebe4b3670c5c04adae674bd92b
bread133 <76678991+bread133@users.noreply.github.com>
2023-08-17 11:37:20 +0300

    block resize of window

--- src/Render/Window.cpp
+++ src/Render/Window.cpp
@@ -10,7 +10,7 @@ int Window::initialize(int width, int height, const char* title)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     window = glfwCreateWindow(width, height, title, NULL, NULL);
     glfwMakeContextCurrent(window);
