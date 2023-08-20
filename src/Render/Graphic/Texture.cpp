70bbff3b64575d5e2b9617bb1a62ffe5b2a532f8
bread133 <76678991+bread133@users.noreply.github.com>
2023-08-20 19:55:35 +0300

    add mini-modification (not-important)

--- src/Render/Graphic/Texture.cpp
+++ src/Render/Graphic/Texture.cpp
@@ -1,2 +1,16 @@
 #include "Texture.h"
 
+void Texture::load_texture(const std::string& path)
+{
+	// OpenGL загружает изображение снизу, а нам необходимо загружать текстуру сверху
+	stbi_set_flip_vertically_on_load(true);
+	// Загрузка
+	unsigned char* pixels = stbi_load(std::string(path).c_str(), &width, &heigth, &channels, 0);
+	if (!pixels)
+	{
+		std::cout << "Нек удалось загрузить следующую текстуру: " << path << std::endl;
+		return;
+	}
+	// Освобождение памяти
+	stbi_image_free(pixels);
+}
--- src/Render/Graphic/Texture.h
+++ src/Render/Graphic/Texture.h
@@ -1,6 +1,16 @@
 #pragma once
 
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_ONLY_PNG
+#include "stb_image.h"
+#include <iostream>
+#include <string>
+
 class Texture
 {
+	int width;
+	int heigth;
+	int channels;
 
+	void load_texture(const std::string& path);
 };
