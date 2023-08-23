618a598caa9eaf803004e3ec8efc8aba6bc7b73a
bread133 <76678991+bread133@users.noreply.github.com>
2023-08-23 17:49:13 +0300

    add Texture class (it is work, I think)

--- src/Render/Graphic/Texture.cpp
+++ src/Render/Graphic/Texture.cpp
@@ -1,6 +1,20 @@
 #include "Texture.h"
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_ONLY_PNG
+#include "stb_image.h"
 
-void Texture::load_texture(const std::string& path)
+Texture::Texture()
+{
+	glGenTextures(1, &id);
+	bind();
+}
+
+Texture::~Texture()
+{
+	glDeleteTextures(1, &id);
+}
+
+void Texture::create_texture(const std::string& path)
 {
 	// OpenGL загружает изображение снизу, а нам необходимо загружать текстуру сверху
 	stbi_set_flip_vertically_on_load(true);
@@ -8,9 +22,29 @@ void Texture::load_texture(const std::string& path)
 	unsigned char* pixels = stbi_load(std::string(path).c_str(), &width, &heigth, &channels, 0);
 	if (!pixels)
 	{
-		std::cout << "Нек удалось загрузить следующую текстуру: " << path << std::endl;
+		std::cout << "Не удалось загрузить следующую текстуру: " << path << std::endl;
 		return;
 	}
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, heigth, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
 	// Освобождение памяти
 	stbi_image_free(pixels);
+}
+
+void Texture::bind() 
+{
+	glBindTexture(GL_TEXTURE_2D, id);
+}
+
+void Texture::load(int layout, std::vector<float> data)
+{
+	glVertexAttribPointer(
+		layout,
+		2,
+		GL_FLOAT, 
+		GL_FALSE, 
+		0,
+		(void*)0);
+	glEnableVertexAttribArray(layout);
 }
--- src/Render/Graphic/Texture.h
+++ src/Render/Graphic/Texture.h
@@ -1,16 +1,24 @@
 #pragma once
 
-#define STB_IMAGE_IMPLEMENTATION
-#define STBI_ONLY_PNG
-#include "stb_image.h"
+#include<glad/glad.h>
 #include <iostream>
 #include <string>
+#include <vector>
 
 class Texture
 {
+	unsigned int id;
 	int width;
 	int heigth;
 	int channels;
+public:
+	Texture();
+	~Texture();
+	void create_texture(const std::string& path);
+	void bind();
+	void load(int layout, std::vector<float> data);
+	// нужно создать функции альясинга и конфигураторы
 
-	void load_texture(const std::string& path);
+	Texture(const Texture&) = delete;
+	Texture& operator=(const Texture&) = delete;
 };
