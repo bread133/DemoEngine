6244f914b8e85e8d47d7970b651bcba5449aec21
bread133 <76678991+bread133@users.noreply.github.com>
2023-08-25 17:27:25 +0300

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,6 @@
 #include "Render/Graphic/Camera.h"
 #include "Render/Graphic/Texture.h"
 
-#include <iostream>
-#include <glad/glad.h>
-
 // Window dimensions
 const int WIDTH = 1080, HEIGHT = 720;
 
@@ -14,96 +11,38 @@ int main()
 {
     Window* window = new Window();
     window->initialize(WIDTH, HEIGHT, "Demo");
-    window->colored(0.1f, 0.0f, 1.0f, 0.7f);
+    window->colored(0.3f, 0.3f, 0.3f, 0.7f);
 
     Mesh* mesh = new Mesh();
-    mesh->load_buffer(0, 
+    mesh->load_buffer(0, 3,
         {
-            -1.0f,-1.0f,-1.0f, // Треугольник 1 : начало
-            -1.0f,-1.0f, 1.0f,
-            -1.0f, 1.0f, 1.0f, // Треугольник 1 : конец
-            1.0f, 1.0f,-1.0f, // Треугольник 2 : начало
-            -1.0f,-1.0f,-1.0f,
-            -1.0f, 1.0f,-1.0f, // Треугольник 2 : конец
-            1.0f,-1.0f, 1.0f,
-            -1.0f,-1.0f,-1.0f,
-            1.0f,-1.0f,-1.0f,
-            1.0f, 1.0f,-1.0f,
-            1.0f,-1.0f,-1.0f,
-            -1.0f,-1.0f,-1.0f,
-            -1.0f,-1.0f,-1.0f,
-            -1.0f, 1.0f, 1.0f,
-            -1.0f, 1.0f,-1.0f,
-            1.0f,-1.0f, 1.0f,
-            -1.0f,-1.0f, 1.0f,
-            -1.0f,-1.0f,-1.0f,
-            -1.0f, 1.0f, 1.0f,
-            -1.0f,-1.0f, 1.0f,
-            1.0f,-1.0f, 1.0f,
-            1.0f, 1.0f, 1.0f,
-            1.0f,-1.0f,-1.0f,
-            1.0f, 1.0f,-1.0f,
-            1.0f,-1.0f,-1.0f,
-            1.0f, 1.0f, 1.0f,
-            1.0f,-1.0f, 1.0f,
-            1.0f, 1.0f, 1.0f,
-            1.0f, 1.0f,-1.0f,
-            -1.0f, 1.0f,-1.0f,
-            1.0f, 1.0f, 1.0f,
-            -1.0f, 1.0f,-1.0f,
-            -1.0f, 1.0f, 1.0f,
-            1.0f, 1.0f, 1.0f,
-            -1.0f, 1.0f, 1.0f,
-            1.0f,-1.0f, 1.0f
+            -1.0f,   -1.0f, -1.0f,  // Треугольник 1 : начало
+            -1.0f,   -1.0f, 1.0f,
+            -1.0f,  1.0f,   1.0f
         });
 
-    mesh->load_buffer(1, 
+    Texture* texture = new Texture();
+    texture->create_texture("D://Download/PXaFcQ10a9k.png");
+
+    /*mesh->load_buffer(1, 3,
         {
             0.583f,  0.771f,  0.014f,
             0.609f,  0.115f,  0.436f,
-            0.327f,  0.483f,  0.844f,
-            0.822f,  0.569f,  0.201f,
-            0.435f,  0.602f,  0.223f,
-            0.310f,  0.747f,  0.185f,
-            0.597f,  0.770f,  0.761f,
-            0.559f,  0.436f,  0.730f,
-            0.359f,  0.583f,  0.152f,
-            0.483f,  0.596f,  0.789f,
-            0.559f,  0.861f,  0.639f,
-            0.195f,  0.548f,  0.859f,
-            0.014f,  0.184f,  0.576f,
-            0.771f,  0.328f,  0.970f,
-            0.406f,  0.615f,  0.116f,
-            0.676f,  0.977f,  0.133f,
-            0.971f,  0.572f,  0.833f,
-            0.140f,  0.616f,  0.489f,
-            0.997f,  0.513f,  0.064f,
-            0.945f,  0.719f,  0.592f,
-            0.543f,  0.021f,  0.978f,
-            0.279f,  0.317f,  0.505f,
-            0.167f,  0.620f,  0.077f,
-            0.347f,  0.857f,  0.137f,
-            0.055f,  0.953f,  0.042f,
-            0.714f,  0.505f,  0.345f,
-            0.783f,  0.290f,  0.734f,
-            0.722f,  0.645f,  0.174f,
-            0.302f,  0.455f,  0.848f,
-            0.225f,  0.587f,  0.040f,
-            0.517f,  0.713f,  0.338f,
-            0.053f,  0.959f,  0.120f,
-            0.393f,  0.621f,  0.362f,
-            0.673f,  0.211f,  0.457f,
-            0.820f,  0.883f,  0.371f,
-            0.982f,  0.099f,  0.879f
+            0.327f,  0.483f,  0.844f
+        });*/
+
+    texture->load(2, {
+        0.0f, 0.0f,  // lower-left corner  
+        1.0f, 0.0f,  // lower-right corner
+        0.5f, 1.0f   // top-center corner
         });
     
-    Shader* shader = load("C://Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Shaders/cube.glslv", 
-        "C://Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Shaders/cube.glslf");
+    Shader* shader = load("C://Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Shaders/figure_texture.glslv", 
+        "C://Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Shaders/figure_texture.glslf");
 
     Camera* camera = new Camera();
     GLuint matrix_id = shader->get_uniform_location(camera->get_MVP());
     Mesh::depth_mode();
-    Mesh::cut_mode();
 
     // Игровой цикл
     float last_delta_time = 0;
@@ -117,16 +56,15 @@ int main()
         mesh->bind();
         mesh->draw(12 * 3);
 
+        texture->bind();
+
         window->swap_buffers();
         window->poll_events();
 
         float delta_time = Window::get_delta_time(last_delta_time);
         camera->input(window, delta_time);
 
-        // Выход из программы, оформить в класс window, но как сделать break?
-        // Хотя эта кнопка нужна будет для вызова меню, если я сделаю
-        if (glfwGetKey(window->window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-            std::cout << "ESC is pressed" << std::endl;
+        if (window->is_exit()) {
             break;
         }
     }
