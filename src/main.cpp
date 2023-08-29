b1a0f361bd969567c8bb9888b2647463d8114128
bread133 <76678991+bread133@users.noreply.github.com>
2023-08-29 21:53:42 +0300

    add model loader

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,72 @@
-#include "Render/Window.h"
 #include "Render/Graphic/Shader.h"
-#include "Render/Graphic/Mesh.h"
+#include "Render/Graphic/Model.h"
 #include "Render/Graphic/Camera.h"
-#include "Render/Graphic/Texture.h"
+#include "Render/Window.h"
 
-// Window dimensions
-const int WIDTH = 1080, HEIGHT = 720;
+// settings
+const unsigned int WIDTH = 1024;
+const unsigned int HEIGHT = 720;
 
 int main()
 {
     Window* window = new Window();
     window->initialize(WIDTH, HEIGHT, "Demo");
-    window->colored(0.3f, 0.3f, 0.3f, 0.7f);
+    window->set_resize();
 
-    Mesh* mesh = new Mesh();
-    mesh->load_buffer(0, 3,
-        {
-            -1.0f,   -1.0f, -1.0f,  // Треугольник 1 : начало
-            -1.0f,   -1.0f, 1.0f,
-            -1.0f,  1.0f,   1.0f
-        });
+    Window::depth_test();
 
-    Texture* texture = new Texture();
-    texture->create_texture("D://Download/PXaFcQ10a9k.png");
+    // build and compile shaders
+    // -------------------------
+    Shader* shader = new Shader("C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Shaders/figure_texture.glslv", 
+        "C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Shaders/figure_texture.glslf");
 
-    /*mesh->load_buffer(1, 3,
-        {
-            0.583f,  0.771f,  0.014f,
-            0.609f,  0.115f,  0.436f,
-            0.327f,  0.483f,  0.844f
-        });*/
+    // load models
+    // -----------
+    Model* test_model = new Model("C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Models/test_cube/test_cube.obj",
+        false);
+    test_model->set_flip_vertically_img();
 
-    texture->load(2, {
-        0.0f, 0.0f,  // lower-left corner  
-        1.0f, 0.0f,  // lower-right corner
-        0.5f, 1.0f   // top-center corner
-        });
-    
-    Shader* shader = load("C://Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Shaders/figure_texture.glslv", 
-        "C://Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Shaders/figure_texture.glslf");
+    // draw in wireframe
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    Camera* camera = new Camera();
-    GLuint matrix_id = shader->get_uniform_location(camera->get_MVP());
-    Mesh::depth_mode();
+    // timing
+    float delta_time = 0.0f;
+    float last_frame = 0.0f;
 
-    // Игровой цикл
-    float last_delta_time = 0;
-    while (!window->window_is_closed())
+    // camera
+    Camera* camera = new Camera(glm::vec3(0.0f, 0.0f, 6.0f));
+
+    // render loop
+    while (!(window->window_is_closed()))
     {
+        delta_time = Window::get_delta_time(last_frame);
+        camera->input(window, delta_time);
+        if (window->is_exit())
+            break;
+        // render
+        // ------
+        window->colored(0.5f, 0.5f, 0.5f, 1.0f);
         window->clear();
 
-        shader->get_uniform_matrix(matrix_id, camera->get_MVP());
+        // don't forget to enable shader before setting uniforms
         shader->use();
 
-        mesh->bind();
-        mesh->draw(12 * 3);
+        camera->mvp_transformation(WIDTH, HEIGHT, shader);
 
-        texture->bind();
+        test_model->draw(shader);
 
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
         window->swap_buffers();
         window->poll_events();
-
-        float delta_time = Window::get_delta_time(last_delta_time);
-        camera->input(window, delta_time);
-
-        if (window->is_exit()) {
-            break;
-        }
     }
 
-    delete camera;
-    delete mesh;
-    delete shader;
     window->terminate();
-    delete window;
 
+    delete camera;
+    delete test_model;
+    delete shader;
+    delete window;
     return 0;
 }
