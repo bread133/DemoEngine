4810011311cd39906a5541841aa266fa8ff27aac
bread133 <76678991+bread133@users.noreply.github.com>
2023-08-29 21:36:33 +0300

    move function of texture to model-class

--- src/Render/Graphic/Texture.h
+++ src/Render/Graphic/Texture.h
@@ -1,24 +1,5 @@
-#pragma once
-
-#include<glad/glad.h>
-#include <iostream>
-#include <string>
-#include <vector>
-
-class Texture
-{
-	unsigned int id;
-	int width;
-	int heigth;
-	int channels;
-public:
-	Texture();
-	~Texture();
-	void create_texture(const std::string& path);
-	void bind();
-	void load(int layout, std::vector<float> data);
-	// нужно создать функции альясинга и конфигураторы
-
-	Texture(const Texture&) = delete;
-	Texture& operator=(const Texture&) = delete;
+struct Texture {
+    unsigned int id;
+    std::string type;
+    std::string path;
 };
