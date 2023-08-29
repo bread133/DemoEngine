3fdb17a1490f573e00e5bb102025f94ed4137756
bread133 <76678991+bread133@users.noreply.github.com>
2023-08-29 21:43:37 +0300

    add exception to shader-class

--- src/Render/Graphic/Shader.cpp
+++ src/Render/Graphic/Shader.cpp
@@ -7,126 +7,107 @@
 #include <fstream>
 #include <vector>
 
-Shader::Shader(unsigned int id) : id(id) {
+Shader::Shader(const char* vertexPath, const char* fragmentPath)
+{
+	load(vertexPath, fragmentPath);
 }
 
 Shader::~Shader() {
 	glDeleteProgram(id);
 }
 
-void Shader::use() {
+void Shader::use()
+{
 	glUseProgram(id);
 }
 
-int Shader::load_shaders(const char* vertex_file_path, const char* fragment_file_path) {
-
-	// Создаем шейдеры
-	GLuint vertex_shader_id = glCreateShader(GL_VERTEX_SHADER);
-	GLuint fragment_shader_id = glCreateShader(GL_FRAGMENT_SHADER);
-
-	// Считываем вертексный шейдер из файла, указанного в виде пути в vertex_file_path
-	std::string vertex_shader_code;
-	std::ifstream vertex_shader_stream(vertex_file_path, std::ios::in);
-	if (vertex_shader_stream.is_open()) {
-		std::stringstream sstr;
-		sstr << vertex_shader_stream.rdbuf();
-		vertex_shader_code = sstr.str();
-		vertex_shader_stream.close();
-	}
-	else {
-		printf_s("Impossible to open %s. Are you in the right directory ? Don't forget to read the FAQ !\n", vertex_file_path);
-		getchar();
-		return 0;
-	}
-
-	// Считываем фрагментный шейдер из файла, указанного в виде пути в fragment_file_path
-	std::string fragment_shader_code;
-	std::ifstream fragment_shader_stream(fragment_file_path, std::ios::in);
-	if (fragment_shader_stream.is_open()) {
-		std::stringstream sstr;
-		sstr << fragment_shader_stream.rdbuf();
-		fragment_shader_code = sstr.str();
-		fragment_shader_stream.close();
-	}
-	else {
-		printf_s("Impossible to open %s. Are you in the right directory ? Don't forget to read the FAQ !\n", fragment_file_path);
-		getchar();
-		return 0;
-	}
-
-	GLint result = GL_FALSE;
-	int info;
-
-	// Компиляция вертексного шейдера
-	printf_s("Compiling shader : %s\n", vertex_file_path);
-	char const* vertex_source_pointer = vertex_shader_code.c_str();
-	glShaderSource(vertex_shader_id, 1, &vertex_source_pointer, NULL);
-	glCompileShader(vertex_shader_id);
-
-	// Тест-чек вертексного шейдера
-	glGetShaderiv(vertex_shader_id, GL_COMPILE_STATUS, &result);
-	glGetShaderiv(vertex_shader_id, GL_INFO_LOG_LENGTH, &info);
-	if (info > 0) {
-		std::vector<char> VertexShaderErrorMessage(info + 1);
-		glGetShaderInfoLog(vertex_shader_id, info, NULL, &VertexShaderErrorMessage[0]);
-		printf_s("%s\n", &VertexShaderErrorMessage[0]);
-	}
-
-	// Компиляция фрагментного шейдера
-	printf_s("Compiling shader : %s\n", fragment_file_path);
-	char const* fragment_source_pointer = fragment_shader_code.c_str();
-	glShaderSource(fragment_shader_id, 1, &fragment_source_pointer, NULL);
-	glCompileShader(fragment_shader_id);
-
-	// Тест-чек фрагментного шейдера
-	glGetShaderiv(fragment_shader_id, GL_COMPILE_STATUS, &result);
-	glGetShaderiv(fragment_shader_id, GL_INFO_LOG_LENGTH, &info);
-	if (info > 0) {
-		std::vector<char> fragment_shader_error_mes(info + 1);
-		glGetShaderInfoLog(fragment_shader_id, info, NULL, &fragment_shader_error_mes[0]);
-		printf_s("%s\n", &fragment_shader_error_mes[0]);
-	}
-
-	// Линковка программы шейдера
-	printf_s("Linking program\n");
-	GLuint program_id = glCreateProgram();
-	glAttachShader(program_id, vertex_shader_id);
-	glAttachShader(program_id, fragment_shader_id);
-	glLinkProgram(program_id);
-
-	// Проверка программы шейдера
-	glGetProgramiv(program_id, GL_LINK_STATUS, &result);
-	glGetProgramiv(program_id, GL_INFO_LOG_LENGTH, &info);
-	if (info > 0) {
-		std::vector<char> program_error_mes(info + 1);
-		glGetProgramInfoLog(program_id, info, NULL, &program_error_mes[0]);
-		printf_s("%s\n", &program_error_mes[0]);
-	}
-
-	// Отключаем шейдеры
-	glDetachShader(program_id, vertex_shader_id);
-	glDetachShader(program_id, fragment_shader_id);
-
-	// Удаляем шейдеры
-	glDeleteShader(vertex_shader_id);
-	glDeleteShader(fragment_shader_id);
-
-	return program_id;
-}
-// Получить хэндл переменной в шейдере
-GLuint Shader::get_uniform_location(glm::mat4 MVP)
+// утилиты для загрузки в файл шейдера
+void Shader::set_mat4(const std::string& name, const glm::mat4& mat) const
 {
-	GLuint matrix_id = glGetUniformLocation(id, "MVP");
-	return matrix_id;
-}
-// Передать наши трансформации в текущий шейдер
-// Это делается в основном цикле, поскольку каждая модель будет иметь другую MVP-матрицу (как минимум часть M)
-void Shader::get_uniform_matrix(GLuint matrix_id, glm::mat4 MVP)
-{
-	glUniformMatrix4fv(matrix_id, 1, GL_FALSE, &MVP[0][0]);
+	glUniformMatrix4fv(glGetUniformLocation(id, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 }
 
-Shader* load(const char* vertex_file_path, const char* fragment_file_path)
+
+void Shader::load(const char* vertexPath, const char* fragmentPath)
 {
-	return new Shader(Shader::load_shaders(vertex_file_path, fragment_file_path));
+	// 1. retrieve the vertex/fragment source code from filePath
+	std::string vertex_code;
+	std::string fragment_code;
+	std::string geometry_code;
+	std::ifstream vertex_shader_file;
+	std::ifstream fragment_shader_file;
+	std::ifstream gShaderFile;
+	// ensure ifstream objects can throw exceptions:
+	vertex_shader_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	fragment_shader_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	try
+	{
+		// open files
+		vertex_shader_file.open(vertexPath);
+		fragment_shader_file.open(fragmentPath);
+		std::stringstream vertex_shader_stream, fragment_shader_stream;
+		// read file's buffer contents into streams
+		vertex_shader_stream << vertex_shader_file.rdbuf();
+		fragment_shader_stream << fragment_shader_file.rdbuf();
+		// close file handlers
+		vertex_shader_file.close();
+		fragment_shader_file.close();
+		// convert stream into string
+		vertex_code = vertex_shader_stream.str();
+		fragment_code = fragment_shader_stream.str();
+	}
+	catch (std::ifstream::failure& e)
+	{
+		std::cout << "ERROR::SHADER::ФАЙЛ НЕ СЧИТЫВАЕТСЯ: " << e.what() << std::endl;
+	}
+	const char* vertex_shader_code = vertex_code.c_str();
+	const char* fragment_shader_code = fragment_code.c_str();
+	// 2. compile shaders
+	unsigned int vertex, fragment;
+	// vertex shader
+	vertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex, 1, &vertex_shader_code, NULL);
+	glCompileShader(vertex);
+	checkCompileErrors(vertex, "VERTEX");
+	// fragment Shader
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment, 1, &fragment_shader_code, NULL);
+	glCompileShader(fragment);
+	checkCompileErrors(fragment, "FRAGMENT");
+	// shader Program
+	id = glCreateProgram();
+	glAttachShader(id, vertex);
+	glAttachShader(id, fragment);
+	glLinkProgram(id);
+	checkCompileErrors(id, "PROGRAM");
+
+	glDetachShader(id, fragment);
+	glDetachShader(id, vertex);
+
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+}
+
+void Shader::checkCompileErrors(GLuint shader, std::string type)
+{
+	GLint success;
+	GLchar info_log[1024];
+	if (type != "PROGRAM")
+	{
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(shader, 1024, NULL, info_log);
+			std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << info_log << "\n -- --------------------------------------------------- -- " << std::endl;
+		}
+	}
+	else
+	{
+		glGetProgramiv(shader, GL_LINK_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(shader, 1024, NULL, info_log);
+			std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << info_log << "\n -- --------------------------------------------------- -- " << std::endl;
+		}
+	}
 }
--- src/Render/Graphic/Shader.h
+++ src/Render/Graphic/Shader.h
@@ -1,21 +1,26 @@
 #pragma once
 
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
 #include <string>
-#include <glm/mat4x4.hpp>
-#include<glad/glad.h>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 
-class Shader 
+class Shader
 {
+    void load(const char* vertexPath, const char* fragmentPath);
+    void checkCompileErrors(GLuint shader, std::string type);
 public:
-	unsigned int id;
+    unsigned int id;
 
-	Shader() = delete;
-	Shader(unsigned int id);
-	~Shader();
-	void use();
-	static int load_shaders(const char* vertex_file_path, const char* fragment_file_path);
-	GLuint get_uniform_location(glm::mat4 MVP);
-	void get_uniform_matrix(GLuint matrix_id, glm::mat4 MVP);
+    Shader(const char* vertexPath, const char* fragmentPath);
+    ~Shader();
+    void use();
+    void set_mat4(const std::string& name, const glm::mat4& mat) const;
 };
-
-extern Shader* load(const char* vertex_file_path, const char* fragment_file_path);
+#endif
