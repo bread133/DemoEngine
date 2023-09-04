d2ea2efb94cd0e473cf0900a8e76ee536a46e022
bread133 <76678991+bread133@users.noreply.github.com>
2023-09-04 13:53:40 +0300

    add skybox in main class

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "Render/Window.h"
 
 #include "Game/Enemy.h"
+#include "Render/Graphic/Skybox.h"
 
 // settings
 const unsigned int WIDTH = 1024;
@@ -11,68 +12,83 @@ const unsigned int HEIGHT = 720;
 
 int main()
 {
-    Window* window = new Window();
-    window->initialize(WIDTH, HEIGHT, "Demo");
-    window->set_resize();
-
+    // window initialize
+    Window* window = new Window(WIDTH, HEIGHT, "Demo");
+    //window->set_resize();
     Window::depth_test();
+    // Enables Cull Facing
+    glEnable(GL_CULL_FACE);
+    // Keeps front faces
+    //glCullFace(GL_FRONT);
+    // Uses counter clock-wise standard
+    //glFrontFace(GL_CCW);
+    /*-------------------------------------------------------------*/
 
-    // build and compile shaders
-    // -------------------------
-    Shader* shader = new Shader("C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Shaders/figure_texture.glslv", 
+    Shader* object_shader = new Shader("C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Shaders/figure_texture.glslv", 
         "C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Shaders/figure_texture.glslf");
+   Shader* skybox_shader = new Shader("C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Shaders/skybox_shader.glslv",
+        "C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Shaders/skybox_shader.glslf");
 
-    // load models
-    // -----------
-    Model* test_model = new Model("C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Models/test_cube/test_cube.obj",
-        false);
-   Model* test_model_enemy = new Model("C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Models/test_enemy/test_enemy.obj", 
-        false);
-   Enemy* enemy = new Enemy(test_model_enemy, 0.0f, 5.0f, 0.0f);
+    // skybox texture loading
+    std::vector<std::string> faces
+    {
+        "C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Skyboxes/Space/right.jpg",
+        "C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Skyboxes/Space/left.jpg",
+        "C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Skyboxes/Space/top.jpg",
+        "C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Skyboxes/Space/bottom.jpg",
+        "C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Skyboxes/Space/front.jpg",
+        "C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Skyboxes/Space/back.jpg"
+    };
+    Skybox* skybox_cube = new Skybox(faces, skybox_shader);
+    /*------------------------------------------------------------*/
 
-    // Window::polygon_mode();
-    
-    // timing
+    Object* cube = new Object(new Model("C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Models/test_cube/test_cube.obj",
+        false), 0.0f, 0.0f, 0.0f);
+    object_shader->use();
+    object_shader->set_int("skybox", 0);
+
+    skybox_shader->use();
+    skybox_shader->set_int("skybox", 0);
+
+    /*Enemy* enemy = new Enemy(new Model(
+        "C:/Users/bread/source/repos/DemoEngineWithCMake/DemoEngine/src/Render/Resources/Models/test_cube/test_cube.obj",
+        false), 0.0f, 20.0f, 0.0f);*/
+
+    //Window::polygon_mode();
+
     float delta_time = 0.0f;
     float last_frame = 0.0f;
+    Camera* camera = new Camera(glm::vec3(0.0f, 5.0f, 5.0f), true);
 
-    // camera
-    Camera* camera = new Camera(glm::vec3(0.0f, 0.0f, 0.0f));
-
-    // render loop
     while (!(window->window_is_closed()))
     {
         delta_time = Window::get_delta_time(last_frame);
         camera->input(window, delta_time);
         if (window->is_exit())
             break;
-        // render
-        // ------
+
         window->colored(0.5f, 0.5f, 0.5f, 1.0f);
         window->clear();
 
-        // don't forget to enable shader before setting uniforms
-        shader->use();
+        object_shader->use();
+        camera->mvp_transformation(WIDTH, HEIGHT, object_shader, glm::vec3(0.0f));
+        cube->draw(object_shader);
+        //enemy->draw(shader, WIDTH, HEIGHT, camera);
+        skybox_cube->draw(skybox_shader, camera, WIDTH, HEIGHT, glm::vec3(0.0f), glm::vec3(10.0f, 10.0f, 10.0f));
 
-        camera->mvp_transformation(WIDTH, HEIGHT, shader);
-
-        test_model->draw(shader);
-        enemy->draw(shader);
-
-
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
         window->swap_buffers();
         window->poll_events();
     }
 
     window->terminate();
 
-    delete enemy;
+    std::cin.get();
+    //delete enemy;
+    delete skybox_cube;
+    delete skybox_shader;
     delete camera;
-    delete test_model;
-    delete shader;
+    delete cube;
+    delete object_shader;
     delete window;
-
     return 0;
 }
