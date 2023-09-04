164a821af991567993bc337c68c7d209cb6f7f5b
bread133 <76678991+bread133@users.noreply.github.com>
2023-09-04 13:53:12 +0300

    add enemy class but it also doesn't work

--- src/Game/Enemy.cpp
+++ src/Game/Enemy.cpp
@@ -1,73 +1,21 @@
 #include "Enemy.h"
 
-void Enemy::move()
-{
-	float min_y = model->mesh->vertices[0].position.y;
-	for (int i = 1; i < model->mesh->vertices.size(); i++)
-		if (min_y > model->mesh->vertices[i].position.y)
-			min_y = model->mesh->vertices[i].position.y;
-
-	std::vector<glm::vec3> down_left_dots;
-	for (int i = 0; i < model->mesh->vertices.size(); i++)
-		if (min_y == model->mesh->vertices[i].position.y)
-			down_left_dots.push_back(model->mesh->vertices[i].position);
-
-	glm::vec3 down_left_dot;
-	if (down_left_dots.size() == 1)
-		down_left_dot = down_left_dots[0];
-	else
-	{
-		float max_z = down_left_dots[0].z;
-		for (int i = 1; i < down_left_dots.size(); i++)
-			if (max_z < down_left_dots[i].z)
-				max_z = down_left_dots[i].z;
-		for(int i = down_left_dots.size() - 1; i > -1; i--)
-			if(down_left_dots[i].z != max_z)
-				down_left_dots.erase(down_left_dots.begin() + i);
-
-		if (down_left_dots.size() == 1)
-			down_left_dot = down_left_dots[0];
-		else
-		{
-			float max_y = down_left_dots[0].y;
-			int index = 0;
-			for (int i = 1; i < down_left_dots.size(); i++)
-				if (max_z < down_left_dots[i].y)
-				{
-					max_z = down_left_dots[i].y;
-					index = i;
-				}
-			down_left_dot = down_left_dots[index];
-		}
-	}
-
-	glm::vec3 translation = position - down_left_dot;
-	for (int i = 0; i < model->mesh->vertices.size(); i++)
-	{
-		model->mesh->vertices[i].position -= translation;
-	}
-}
-
 Enemy::Enemy(Model* model, float pos_x, float pos_y, float pos_z, float speed, float damage, float hp) :
-	model(model),
+	Object(model, pos_x, pos_y, pos_z),
 	speed(speed),
-	position(glm::vec3(pos_x, pos_y, pos_z)),
 	damage(damage),
 	hp(hp)
 {
 	alive = true;
-	move();
 }
 
 Enemy::Enemy(Model* model, float pos_x, float pos_y, float pos_z) :
-	model(model),
-	position(glm::vec3(pos_x, pos_y, pos_z)),
+	Object(model, pos_x, pos_y, pos_z),
 	speed(SPEED_ENEMY),
 	damage(DAMAGE),
 	hp(HP)
 {
 	alive = true;
-	move();
 }
 
 Enemy::~Enemy()
@@ -75,7 +23,23 @@ Enemy::~Enemy()
 	delete model;
 }
 
-void Enemy::draw(Shader* shader)
+void Enemy::get_position(Camera* camera, float delta_time)
 {
-	model->draw(shader);
+	glm::vec3 goal = camera->get_position();
+	position += goal * delta_time * speed;
+}
+
+void Enemy::draw(Shader* shader, int WIDTH, int HEIGHT, Camera* camera)
+{
+	if (alive)
+	{
+		camera->mvp_transformation(WIDTH, HEIGHT, shader, position);
+		Object::draw(shader);
+	}
+}
+
+void Enemy::is_dead() 
+{
+	if (hp == 0)
+		alive = false;
 }
--- src/Game/Enemy.h
+++ src/Game/Enemy.h
@@ -1,27 +1,28 @@
 #pragma once
 #include "../Render/Graphic/Model.h"
 #include "../Render/Graphic/Shader.h"
+#include "Object.h"
+#include "../Render/Window.h"
+#include "../Render/Graphic/Camera.h"
 
 #include <glm/vec3.hpp>
 #include <vector>
 
-const float SPEED_ENEMY = 2.5f;
-const float DAMAGE = 5.0f;
-const float HP = 100.0f;
+const float			SPEED_ENEMY = 0.5f;
+const float			DAMAGE = 5.0f;
+const int			HP = 100.0f;
 
-class Enemy
+class Enemy : public Object
 {
-	glm::vec3 position;
-	Model* model;
 	float speed;
 	float damage;
-	float hp;
+	signed int hp;
 	bool alive;
-	void move();
+	void get_position(Camera* camera, float delta_time);
 public:
 	Enemy(Model* model, float pos_x, float pos_y, float pos_z, float speed, float damage, float hp);
 	Enemy(Model* model, float pos_x, float pos_y, float pos_z);
 	~Enemy();
-	void draw(Shader *shader);
-	bool is_dead();
+	void draw(Shader* shader, int WIDTH, int HEIGHT, Camera* camera);
+	void is_dead();
 };
